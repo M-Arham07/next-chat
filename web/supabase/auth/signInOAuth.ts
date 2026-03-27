3f696e275a68649c06cdfaa7af6190c7a3a62d57
M-Arham07 <khawajaarham15@gmail.com>
2026-03-27 18:39:57 +0500

    fix incorrect redirect

--- web/supabase/auth/signInOAuth.ts
+++ web/supabase/auth/signInOAuth.ts
@@ -3,10 +3,12 @@ import { createClient } from "../client";
 export async function signInWithOAuth(provider: "google" | "github") {
   const supabase = createClient();
 
+  console.log("BASE URL IS", window.location.origin);
+
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider,
     options: {
-      redirectTo: `${window.location.origin}/api/auth/callback`,
+      redirectTo: `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/callback`,
     },
   });
 
